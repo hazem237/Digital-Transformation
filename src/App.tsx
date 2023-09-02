9feb390090b05d08d2a4e12bf4296bf66b6d9d2d
Hazem Haddad <Hazem.Haddad@foothillsolutions.com>
2023-09-02 19:39:30 +0300

    Logo creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-export default function App() {
+import React from 'react';
+import Logo from '././components/Logo';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div className="App">
+      <Logo />
+    </div>
+  );
 }
+
+export default App;
