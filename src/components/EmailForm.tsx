c0d18e04e311ddedec34cfbde964b6fdb60bccf0
Momen-G-Ar <cp.momen@gmail.com>
2024-01-20 12:43:33 +0200

    Format emailForm & update version

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,42 +1,50 @@
-import React from 'react';
-import emailjs from '@emailjs/browser';
+import React from "react";
+import emailjs from "@emailjs/browser";
 
 const EmailForm = () => {
-
     const form: any = React.useRef();
 
     const handleSendMessage = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        emailjs.sendForm('service_mykfw6x', 'template_k8mllka', form.current, 'lnJFQ7qA2oO5vBzvr')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-        e.currentTarget.Name.value = '';
-        e.currentTarget.Message.value = '';
-        e.currentTarget.Email.value = '';
-    }
+        emailjs
+            .sendForm(
+                "service_mykfw6x",
+                "template_k8mllka",
+                form.current,
+                "lnJFQ7qA2oO5vBzvr"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+        e.currentTarget.Name.value = "";
+        e.currentTarget.Message.value = "";
+        e.currentTarget.Email.value = "";
+    };
 
     return (
         <form
             ref={form as any}
-            onSubmit={(e) => { handleSendMessage(e) }}
+            onSubmit={(e) => {
+                handleSendMessage(e);
+            }}
             className={`
                 grid sm:grid-cols-2 sm:grid-rows-[0.2fr_1fr_0.2fr] 
                 grid-cols-1 grid-rows-[0.7fr_0.7fr_1fr_0.7fr] w-[70%] gap-[0.2rem] sm:gap-4 
                 border-solid border-2 shadow-2xl h-80 lg:h-[80%] rounded-lg p-6 font-medium text-[var(--primary-color)] text-right
             `}
         >
-            <div
-                className='flex flex-col sm:col-span-1 row-span-1 gap-[4px]'
-            >
-                <span className='block'>* الايميل</span>
+            <div className="flex flex-col sm:col-span-1 row-span-1 gap-[4px]">
+                <span className="block">* الايميل</span>
                 <input
                     name="Email"
                     required
-                    type='email'
-                    placeholder='الايميل'
+                    type="email"
+                    placeholder="الايميل"
                     className={`
                         text-right p-[4px]
                         bg-gray-100 border-solid border-[1px] rounded-[3px] font-normal
@@ -44,15 +52,13 @@ const EmailForm = () => {
                     `}
                 />
             </div>
-            <div
-                className='flex flex-col sm:col-span-1 row-span-1 gap-[4px]'
-            >
-                <span className='block'> * الاسم</span>
+            <div className="flex flex-col sm:col-span-1 row-span-1 gap-[4px]">
+                <span className="block"> * الاسم</span>
                 <input
                     name="Name"
                     required
-                    type='text'
-                    placeholder='الاسم'
+                    type="text"
+                    placeholder="الاسم"
                     className={`
                         bg-gray-100 border-solid border-[1px] rounded-[3px] font-normal
                         text-right p-[4px]
@@ -60,13 +66,11 @@ const EmailForm = () => {
                     `}
                 />
             </div>
-            <div
-                className='flex flex-col sm:col-span-2 row-span-1 gap-[4px]'
-            >
-                <span className='block'>* محتوى الرسالة</span>
+            <div className="flex flex-col sm:col-span-2 row-span-1 gap-[4px]">
+                <span className="block">* محتوى الرسالة</span>
                 <textarea
                     name="Message"
-                    placeholder='الرسالة'
+                    placeholder="الرسالة"
                     required
                     className={`
                         bg-gray-100 border-solid border-[1px] rounded-[3px] resize-none flex-grow font-normal text-right p-[8px]
@@ -74,7 +78,7 @@ const EmailForm = () => {
                     `}
                 />
             </div>
-            <div className='flex flex-col sm:col-span-2 row-span-1 justify-center'>
+            <div className="flex flex-col sm:col-span-2 row-span-1 justify-center">
                 <button
                     type="submit"
                     className={`
@@ -87,7 +91,7 @@ const EmailForm = () => {
                 </button>
             </div>
         </form>
-    )
-}
+    );
+};
 
 export default EmailForm;
