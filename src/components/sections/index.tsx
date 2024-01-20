0913dcd1daf1e9bfc48bd3cc40eb60ccb7cbb9c3
Momen-G-Ar <cp.momen@gmail.com>
2024-01-20 12:46:34 +0200

    Edit names

--- src/components/sections/index.tsx
+++ src/components/sections/index.tsx
@@ -1,5 +1,5 @@
-export { default as Strategy } from './strategy';
-export { default as HomePage } from './home-page';
-export { default as AboutUs } from './aboutus';
-export { default as ContactUs } from './contactus';
-export { default as OurServices } from './our-services';
+export { default as Strategy } from "./strategy";
+export { default as HomePage } from "./home-page";
+export { default as AboutUs } from "./aboutus";
+export { default as ContactUs } from "./contactus";
+export { default as OurServices } from "./our-services";
